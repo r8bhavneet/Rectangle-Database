788135e2188b169c5b5ddb84f128ca3e5db732ff
Bhavneet Singh <bsr1@sfu.ca>
2022-02-13 11:16:35 -0800

    some changes

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require('express');
 const { redirect } = require('express/lib/response');
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
 const{ Pool}= require('pg');
 var pool;
 pool= new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://postgres:Reset123@localhost/rectangles'
+    connectionString: process.env.DATABASE_URL || 'postgres://postgres:Reset123@localhost/rectangles',
+    ssl: {
+        rejectUnauthorized: false
+      }
 })
 
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/database'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
   app.get('/database',(req,res)=>{
